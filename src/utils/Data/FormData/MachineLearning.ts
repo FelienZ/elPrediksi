fc83a92c916a5e453d647e0f6e71c42496700489
FelienZ <rajabfadh@gmail.com>
2025-11-29 17:13:58 +0700

    Change how defaultValue/options created for dummmy MLData

--- src/utils/Data/FormData/MachineLearning.ts
+++ src/utils/Data/FormData/MachineLearning.ts
@@ -1,22 +1,15 @@
 import type { ML } from "../../types/FormData/MachineLearning";
+import type { OptionType } from "../../types/OptionTypes";
 
+const defaultOption: OptionType = {
+    value: -1,
+    label: ''
+}
 // dummy
 export const MLData: ML = {
-    pH: 0,
-    Color: {
-        value:0,
-        label: 'Colorless'
-    },
-    Turbidity: {
-        value:0,
-        label: 'Jernih'
-    },
-    Odor: {
-        value:0,
-        label: 'Bau'
-    },
-    Source: {
-        value:0,
-        label: 'Aquifer'
-    },
+    pH: NaN,
+    Color: defaultOption,
+    Turbidity: defaultOption,
+    Odor: defaultOption,
+    Source: defaultOption,
 }
