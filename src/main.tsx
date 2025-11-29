147dd018531fb92ac1fbab78013167bbd67030f6
FelienZ <rajabfadh@gmail.com>
2025-11-29 17:20:14 +0700

    implement theme Provider in root

--- src/main.tsx
+++ src/main.tsx
@@ -7,18 +7,21 @@ import Template from './frontend/Template/Template'
 import MachineLearning from './frontend/Home/ML/main'
 import DataScience from './frontend/Home/DS/main'
 import Notfound from './NotFound'
+import ThemeProvider from './utils/context/ThemeProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
+      <ThemeProvider>
+        <Routes>
         <Route path='/' element={<Template/>}>
-          <Route index element={<Navigate to={'ml'} replace/>}/>
-          <Route path='ml' element={<MachineLearning/>}/>
-          <Route path='ds' element={<DataScience/>}/>
-          <Route path='*' element={<Notfound/>}/>
-        </Route>
-      </Routes>
+            <Route index element={<Navigate to={'ml'} replace/>}/>
+            <Route path='ml' element={<MachineLearning/>}/>
+            <Route path='ds' element={<DataScience/>}/>
+            <Route path='*' element={<Notfound/>}/>
+          </Route>
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
 )
