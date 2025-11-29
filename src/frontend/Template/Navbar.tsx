0544eb67e4049ab89c7502029e8e76bb0ad5ce77
FelienZ <rajabfadh@gmail.com>
2025-11-29 17:19:31 +0700

    Implement Theme Switcher & framer transtition

--- src/frontend/Template/Navbar.tsx
+++ src/frontend/Template/Navbar.tsx
@@ -3,10 +3,14 @@ import { NavLink, useLocation } from 'react-router';
 import { NavbarItems } from '../../utils/Data/Navbar/Items';
 import type { NavbarTypes } from '../../utils/types/NavbarTypes';
 import { motion } from "motion/react"
+import { Button } from '../../components/ui/button';
+import useTheme from '../../utils/hooks/useTheme';
+import { faLightbulb, faMoon } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar(){
     const location = useLocation()
-    const Pages: NavbarTypes = NavbarItems.find(n => location.pathname === '/ds' ? n.title === 'Movie Preferences' : n.title === 'Water Quality')!
+    const {theme, setTheme} = useTheme()
+    const Pages: NavbarTypes = NavbarItems.find(n => location.pathname === '/ds' ? n.title === 'Movie Preferences' : n.title === 'Water Quality Checker')!
     return(
         <section className="flex w-screen fixed z-20 top-0 bg-card text-card-foreground drop-shadow-sm justify-between p-4">
             <motion.div 
@@ -35,6 +39,19 @@ export default function Navbar(){
                 <NavLink className={({isActive}) => isActive ? 'underline transition-opacity duration-400 ease-in-out underline-offset-4 text-shadow-md opacity-100' : 'opacity-60 text-shadow-md'} to={'ml'}><p>ML</p></NavLink>
                 <p>|</p>
                 <NavLink className={({isActive}) => isActive ? 'underline transition-opacity duration-400 ease-in-out underline-offset-4 text-shadow-md opacity-100' : 'opacity-60 text-shadow-md'} to={'ds'}><p>DS</p></NavLink>
+                <Button onClick={()=>setTheme()} className={`rounded-full transition-colors ease-in-out duration-300 flex w-15 px-1 ${theme === 'dark' ? 'justify-end bg-neutral-700 hover:bg-neutral-800' : 'justify-start bg-neutral-200 hover:bg-neutral-300'}`}>
+                    <motion.div
+                        className={`size-7 rounded-full transition-colors duration-200 ease-in-out ${theme === 'light' ? 'bg-neutral-300 text-neutral-600' : 'bg-neutral-800 text-white'} items-center justify-center flex my-0`}
+                        layout
+                        transition={{
+                            type: "spring",
+                            visualDuration: 0.2,
+                            bounce: 0.2,
+                        }}
+                    >
+                      {theme === 'light' ? <FontAwesomeIcon icon={faLightbulb}/> : <FontAwesomeIcon icon={faMoon}/>}
+                      </motion.div>
+                    </Button>
             </motion.div>
         </section>
     )
--- src/frontend/Template/Template.tsx
+++ src/frontend/Template/Template.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router";
 
 export default function Template(){
     return(
-        <section className="min-h-screen font-[Roboto] bg-background text-foreground">
+        <section className="min-h-screen font-[Roboto] bg-background text-foreground transition-colors duration-300">
             <Navbar/>
                 <main className="pt-20 pb-5 px-5">
                     <Outlet/>
