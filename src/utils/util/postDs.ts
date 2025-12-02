2900ebc8a11c9bfd293cbb29c64cd3ce3c26d6b3
FelienZ <rajabfadh@gmail.com>
2025-12-02 22:00:21 +0700

    Add post Request to get recommendations movie from server

---
+++ src/utils/util/postDs.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import type { DS } from "../types/DS/DataScience";
+import type { Dispatch, SetStateAction } from "react";
+import type { Recommendations } from "../types/DS/Recommend";
+import { nanoid } from 'nanoid'
+const api_key = import.meta.env.VITE_API_KEY
+
+export default async function PostDSData(query: DS[], setIsLoading: Dispatch<SetStateAction<boolean>>, setRecommendations: Dispatch<SetStateAction<Recommendations[]>>, setQuery: Dispatch<SetStateAction<DS[]>>){
+    setIsLoading(true)
+    try {
+        const {data: response} = await axios.post('http://localhost:5000/recommend', query)
+        const MovieData = await Promise.all(response.recommendations?.map(async(r: Recommendations) => {
+            return{
+                title: r.title,
+                poster: await axios.get(`https://omdbapi.com/?t=${r.title}&apikey=${api_key}`).then(r =>r.data?.Poster?? '/images/not-found.jpg'),
+                id: `Poster-${nanoid()}`
+            }
+        }))
+        setQuery([])
+        setRecommendations(MovieData)
+    } catch (error) {
+        console.log(error)
+    }finally{
+        setIsLoading(false)
+    }
+}
