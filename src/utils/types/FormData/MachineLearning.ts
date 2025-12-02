7615dd6b91888785fa281952503d8e2e39aa55bb
FelienZ <rajabfadh@gmail.com>
2025-12-02 22:07:56 +0700

    sync progress for env & assets

--- src/utils/types/FormData/MachineLearning.ts
+++
@@ -1,9 +0,0 @@
-import type { OptionType } from "../OptionTypes"
-
-export interface ML {
-    pH: number,
-    Color: OptionType
-    Turbidity: OptionType
-    Odor: OptionType
-    Source:OptionType
-}
--- src/utils/types/FormData/Submit.ts
+++
@@ -1,8 +0,0 @@
-// inputData
-export type MLSubmit = {
-    pH: number,
-    Odor: number,
-    Color: number,
-    Turbidity: number,
-    Source: number
-}
